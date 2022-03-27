3d10a0f3de58d294beea2665b8ed50c02927c4a0
karthik-d <karthikdesingu2000@gmail.com>
2022-03-27 07:57:57 +0530

    output refined. dir struct modified

--- Meet-6/Server-ML/app.py
+++ Meet-6/Server-ML/app.py
@@ -16,23 +16,19 @@ def home():
     return render_template('index.html', prediction_text='Predicted salary: $ {}'.format(10))
 
 
-@app.route('/predict',methods=['POST'])
+@app.route('/',methods=['POST'])
 def predict():
-
-    # float_features = [float(x) for x in request.form.values()]
-    # final_features = [np.array(float_features)]
-    # prediction = model.predict(final_features)
-
-    # output = round(prediction[0], 2)
-    output = 10
+    # Get image from POST data
     imagefile = request.files.get('imagefile', '')
-    imagefile.save('test.png')
-    np_img = cv2.imread('test.png', cv2.IMREAD_GRAYSCALE)
+    imagefile.save('static/images/input_img.png')
+    # Read image into numpy array
+    np_img = cv2.imread('static/images/input_img.png', cv2.IMREAD_GRAYSCALE)
     np_img = np_img/255
     np_img_1d = np_img.reshape(-1, 1024)
+    # Predict and Display
     digit_predict = np.argmax(model.predict(np_img_1d))
-    prediction_text = "Couldn't find a digit" if digit_predict==0 else f"That's a {digit_predict}"
-    return render_template('index.html', prediction_text=prediction_text)
+    prediction_text = "Couldn't find a digit there" if digit_predict==0 else f"That's a {digit_predict}"
+    return render_template('index.html', prediction_text=prediction_text, display_img='images/input_img.png')
 
 
 @app.route('/results',methods=['POST'])
